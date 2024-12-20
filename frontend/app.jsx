69bccdbbdbae241143751db6020d39f68d37ccb1
Harsh Nesari <harshnesari9@gmail.com>
2024-12-20 09:45:19 +0530

    front end setup

---
+++ frontend/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from "react-dom/client"
+import Server from './server.jsx'
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+       <Server/>
+    </React.StrictMode>
+)
+
---
+++ frontend/server.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Server() {
+  return (
+    <>
+      <h1 className='text-orange-800'>hello sir</h1>
+    </>
+  )
+}
+
+export default Server
