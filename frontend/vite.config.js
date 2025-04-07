07f187fe3efefd399d475c6c3b567eecb846e82b
aishaa02 <02fe22bcs009@kletech.ac.in>
2025-04-07 10:51:39 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // or '/subpath/' if hosted at a subdirectory
   plugins: [react()],
 });
